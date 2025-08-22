2c1349b06276faa227fd1f1794621f98fde5b8fc
zoyka7 <sergejzoyka@hotmail.com>
2025-08-22 18:01:30 +0200

    birthday script

--- birthday.ps1
+++ birthday.ps1
@@ -3,8 +3,9 @@ param(
     [datetime]$Birthday
 )
 
-$startYear = $Birthday.Year
-$endYear = (Get-Date).Year + 50
+$startYear = $Birthday.Year 
+$endYear = $Birthday.Year + 100
+#$endYear = (Get-Date).Year + 100
 
 Write-Host "Calcolo dei giorni della settimana per il compleanno dal $startYear al $endYear`n"  
 
@@ -20,19 +21,19 @@ $weekdaysCount = @{
 }
 
 Write-Host "Distribuzione dei giorni della settimana per il compleanno ($($Birthday.ToString('dd/MM/yyyy'))):`n"
-Write-Host "anno partenza: $startYear   anno finale $endYear" -ForegroundColor Yellow
-Write-Host "--------------------------------" -ForegroundColor Yellow       
+Write-Host "anno partenza: $startYear   anno finale $endYear" -ForegroundColor Green
+# Write-Host "--------------------------------" -ForegroundColor Yellow       
 
 
  for ($year = $startYear; $year -le $endYear; $year++) {
      $currentBirthday = Get-Date -Year $year -Month $Birthday.Month -Day $Birthday.Day
      $weekday = $currentBirthday.ToString("dddd", [System.Globalization.CultureInfo]::GetCultureInfo("it-IT"))
      $weekdaysCount[$weekday]++
-     Write-Host "$year : $weekday"
+     Write-Host "$year : $weekday" -ForegroundColor Yellow
  } 
 
  
-Write-Host "`nRiepilogo:" -ForegroundColor Yellow
+Write-Host "`nRiepilogo:" -ForegroundColor Green
 foreach ($day in $weekdaysCount.Keys) {
-    Write-Host "$day : $($weekdaysCount[$day]) volte" -ForegroundColor Green
-} #>
+    Write-Host "$day : $($weekdaysCount[$day]) volte" -ForegroundColor Yellow
+} 
